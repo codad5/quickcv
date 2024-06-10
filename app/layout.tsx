a227b7fc4423ee66a05f3c039a7f1ca5de387c82
Ridox_Studio <66634814+codad5@users.noreply.github.com>
2024-06-10 09:18:08 +0100

    added header

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,27 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <header className="w-full shadow-md flex items-center justify-between px-4 py-6">
+         <h1 className="text-lg font-semibold">
+          QuickCv
+          {/* a credit with link to my github something that is small and say by codad5 then with link to my portfolio codad5.me */}
+          <span className="text-sm ml-2">
+            by <a href="https://github.com/codad5" target="_blank" rel="noopener noreferrer" className="underline text-blue-500">codad5</a>
+            {" | "}
+            <a href="https://codad5.me" target="_blank" rel="noopener noreferrer" className="underline text-blue-500">codad5.me</a>
+          </span>
+        </h1>
+        <nav>
+          <ul className="flex space-x-4">
+            <li>
+              <a href="/about">About</a>
+            </li>
+            <li>
+              <a href="/contact">Contact</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
         {children}
         <Analytics  />
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,7 @@ import ResumeBuilder from './clients/ResumeBuilder';
 
 export default function Home() {
   return (
-    <main className="flex min-h-[200svh] sm:min-h-screen  justify-start sm:justify-center px-4 py-20 gap-4  pb-4 flex-col sm:flex-row">
-      
+    <main className="flex min-h-[200svh] sm:min-h-screen  justify-start sm:justify-center px-4 py-2 gap-4  pb-4 flex-col sm:flex-row">
       <ResumeBuilder />
     </main>
   );
