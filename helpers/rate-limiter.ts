4ae21f1015a5c59265e6722a4b372527fab0815d
codad5 <aniezeoformic@gmail.com>
2024-09-13 11:52:53 +0100

    refactor: Update .env.sample and rate-limiter.ts
    - Update QUICK_CV_RATE_LIMIT in .env.sample to 3 requests per 15 minutes
    - Add QUICK_CV_RATE_LIMIT_DURATION in .env.sample with a value of 15 minutes
    - Update RATE_LIMIT_COUNT and limiter duration in rate-limiter.ts to reflect the new rate limit

--- helpers/rate-limiter.ts
+++ helpers/rate-limiter.ts
@@ -3,10 +3,11 @@ import Redis from "./redis";
 import { getIp } from "./commons/server";
 
 const RATE_LIMIT_COUNT = parseInt(process.env.QUICK_CV_RATE_LIMIT ?? "20");
+const RATE_LIMIT_DURATION = parseInt(process.env.QUICK_CV_RATE_LIMIT_DURATION ?? "60")
 
 export const ResumeGeneratorLimiter = new Ratelimit({
     redis: Redis,
-    limiter: Ratelimit.slidingWindow(RATE_LIMIT_COUNT, "60 m"),
+    limiter: Ratelimit.slidingWindow(RATE_LIMIT_COUNT, `${RATE_LIMIT_DURATION} m`),
     analytics: true,
     prefix: "ratelimit:resume-generator",
 });
