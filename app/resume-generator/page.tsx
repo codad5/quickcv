cd74625e950c8dd27c1fb713924ceb3bf7982f17
codad5 <aniezeoformic@gmail.com>
2024-09-14 02:28:31 +0100

    Refactor layout for resume generator page; adjust minimum height for better responsiveness

--- app/resume-generator/page.tsx
+++ app/resume-generator/page.tsx
@@ -15,7 +15,7 @@ export default async function Home() {
       <main className="">
         <RememberMe app_name="resumeInfo" />
         <CreditUsage remains={remains} max={MAX_CREDIT} />
-        <div className="flex min-h-[200svh] sm:min-h-screen  justify-start sm:justify-center px-4 py-2 gap-4  pb-4 flex-col sm:flex-row">
+        <div className="flex  sm:h-screen  justify-start sm:justify-center px-4 py-2 gap-4  pb-4 flex-col sm:flex-row">
             <ResumeBuilder />
         </div>
       </main>
