c08d05ca3911cf8ac76095247f773c775ef0fd49
codad5 <aniezeoformic@gmail.com>
2024-09-14 02:19:36 +0100

    Replace checkbox with toggle button for "Remember Me" feature; update icons for better UX

--- components/commons/RememberMe.tsx
+++ components/commons/RememberMe.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { APPToRemember, isRemembered, updateRememberMe } from "@/helpers/commons/client";
+import { ToggleOffCircle, ToggleOnCircle } from "iconsax-react";
 import { useState, useEffect } from "react";
 
 function RememberMe({
@@ -23,11 +24,13 @@ function RememberMe({
 
   return (
     <div className="flex items-center justify-center">
-      <input
-        type="checkbox"
-        checked={rememberMe}
-        onChange={(e) => setRememberMe(e.target.checked)}
-      />
+      <button onClick={() => setRememberMe(!rememberMe)}>
+        {rememberMe ? (
+          <ToggleOnCircle color="green" size="32" />
+        ) : (
+          <ToggleOffCircle color="red" size="32" />
+        )}
+      </button>
       <label className="ml-2">Remember me</label>
     </div>
   );
