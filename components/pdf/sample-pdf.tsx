e98de9fe3d83e0ae96fdff5136b895e5166cd091
codad5 <aniezeoformic@gmail.com>
2024-10-16 19:34:41 +0100

    Refactor layout component to include a progress bar section

--- components/pdf/sample-pdf.tsx
+++ components/pdf/sample-pdf.tsx
@@ -1,6 +1,7 @@
+"use client";
+import { useEffect, useState } from "react";
 import Markdown from "react-markdown";
 import remarkGfm from "remark-gfm";
-
 
 const SAMPLE_PDF = `
 **Chineye Igwe**  
@@ -41,25 +42,34 @@ Lagos State University, 2017
 ### **References**  
 Available upon request
 
----
-
-### **3. Final Suggestions**
-- **Consistent Layout:** Make sure all sections are well-aligned, and don't forget about small visual cues like dividers, icons (like email icons), or subtle separators between fields.
-- **White Space:** Try adding a bit more padding in the preview section to give each section room to breathe.
-- **Color Ideas:** If you want to make your design more engaging, consider using **section headers in light blue** and keeping text dark for better readability.
-
-
 `;
 
 export default function SamplePDF() {
-    return (
-      <div className="pdf-sample w-[760px] max-w-full mx-auto h-[590px] max-h-1/2 bg-white px-6 py-10">
-        <Markdown
-          remarkPlugins={[remarkGfm]}
-          className="prose prose-lg prose-zinc w-full max-w-none"
-        >
-          {SAMPLE_PDF}
-        </Markdown>
-      </div>
-    );
+  const [viewRaw, setViewRaw] = useState(``);
+  const TOTAL_TIME = 8000; // Change this value to increase or decrease speed
+
+  // typing effect
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setViewRaw((prev) => {
+        if (prev.length === SAMPLE_PDF.length) {
+          clearInterval(interval);
+          return prev; // Stop updating when complete
+        }
+        return SAMPLE_PDF.slice(0, prev.length + 1);
+      });
+    }, TOTAL_TIME/SAMPLE_PDF.length);
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className="pdf-sample w-[760px] max-w-full mx-auto h-[590px] max-h-1/2 bg-white px-6 py-10">
+      <Markdown
+        remarkPlugins={[remarkGfm]}
+        className="prose prose-lg prose-zinc w-full max-w-none"
+      >
+        {viewRaw}
+      </Markdown>
+    </div>
+  );
 }
